ee9610506153791fa3f5c7411f29bc720c711e93
shashi.dwivedi <shashi.dwivedi@quantumcs.com>
2022-07-22 19:00:35 +0530

    Cron job notification Local phase2-2

--- vcimsweb/adminpanel/mangopinion/cron.php
+++ vcimsweb/adminpanel/mangopinion/cron.php
@@ -1,99 +1,110 @@
 <?php
-
 ///////////////////////////////DATABASE CONNECTION ///////////////////
         $dbhost = 'localhost';
          $dbuser = 'root';
          $dbpass = '';
 		 $dbname = 'vcims';
-
-		//  $con = mysqli_connect($dbhost,$dbuser ,$dbpass,$dbname);
-
-        //     if (mysqli_connect_errno())
-        //     {
-        //         echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        //     }
-
-
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         $mysqli = new mysqli($dbhost,  $dbuser,  $dbpass,  $dbname);
-
-
-
-
-    echo "<center><font color=red><h2>CRON JOB SEDULING FOR NOTIFICATION</h2></font>";    
 //////////////////////////////END DATA BASE CONNECTION ////////////////////
 
+
+/////////////////////INSERT DATA INTO CRON JOB TABLE///////////////////////
+if(isset($_POST['createCronJob'])){ 
+$bankFlag0 =0;$bankFlag1 =0;$bankFlag5 =0;$bankFlag6 =0;$bankFlag7 =0;$bankFlag4 =0;
+   if(isset($_POST['projectId'])){ $projId= $_POST['projectId'];} else{$bankFlag0 =1;}
+   if(isset($_POST['projectStatus'])){ $projStatus= $_POST['projectStatus'];}else{$bankFlag1 =1;}
+   if(isset($_POST['cronHour'])){$cronHour= $_POST['cronHour'];}else{$bankFlag5 =1;}
+   if(isset($_POST['cronMinuts'])){$cronminute= $_POST['cronMinuts'];}else{$bankFlag6 =1;}
+   if(isset($_POST['cronMessage'])){$cronMessge= $_POST['cronMessage'];}else{$bankFlag7 =1;}
+   if(isset($_POST['timeZone'])){$timeZone= $_POST['timeZone'];}else{$bankFlag4 =1;}
+if($bankFlag0 ==1 || $bankFlag1 ==1 ||  $bankFlag5 ==1 || $bankFlag6 ==1  || $bankFlag7 ==1 || $bankFlag4 ==1 )
+{
+    echo "<center><font color=red><h4>Please fill complete information to set the CRON Job</h4></font>";
+
+}else{
+    $qrr="INSERT INTO `cron_job` ( `project_id`, `project_status`, `hour`, `minutes`, `message`, `tzlist`) VALUES 
+    ($projId, $projStatus, $cronHour, $cronminute,  '$cronMessge','$timeZone')";
+    $result = $mysqli->query($qrr);
+    if($result){
+        echo "<center><font color=green><h4>CRON Job is Successfully Created !</h4></font>";
+    }else{
+        echo "<center><font color=red><h4>Some Technical problem CRON job not created!</h4></font>"; 
+    }
+}
+}
+
+
+
+////////////////////END THE INSERT SECTION IN THE TABLE////////////////////
+
+
+
+echo "<center><font color=red><h2>CRON JOB SEDULING FOR NOTIFICATION</h2></font>"; 
 $qrr="SELECT appuser_project_map.project_id,project.name FROM appuser_project_map INNER JOIN project on appuser_project_map.project_id = project.project_id GROUP by appuser_project_map.project_id";
 $result = $mysqli->query($qrr);
 $prows = $result->fetch_all(MYSQLI_ASSOC);
 
 ?>
 
-<form method=post action="" onsubmit="return submitForm(this);">
-<table border=1; bgcolor="#e8eef3"; style="width:60%">>
+<form method=post action="cron.php"">
+<table border=1; bgcolor="#e8eef3"; style="width:60%">
 
-<tr> <td>Select Project *</td>   <td> <select name=monthName id=monthName><option value=''>-Select Project-</option>
-   <?php 
-   foreach($prows as $rdata){
+<tr> <td>Write Message *</td>
+<td><textarea rows=3 cols=40 name="cronMessage" id="cronMessage" placeholder='Type your message here' required></textarea></td>
+        </tr>
+
+<tr> <td>Select Project *</td>   <td> <select name=projectId id=projectId required><option value=''>-Select Project-</option>
+    
+<?php   foreach($prows as $rdata){
          echo "<option value='";
-        	 echo $rdata['project_id']; 
-        	echo "'>";
-        	 echo $rdata['name']; 
-        	echo "</option>";
-        	  } ?>
-        
+         echo $rdata['project_id']; 
+         echo "'>";
+         echo $rdata['name']; 
+         echo "</option>";
+        	  } ?>  
+        </td> </tr>
+            
+
+
+            <tr> <td>Whom Send Message *</td>   <td> <select name=projectStatus id=projectStatus required><option value=''>-Select Users types-</option>
+            <option value="0">Who Submit The SUrvey</option>
+            <option value="1">Who Not Submited Survey</option>
             </td> </tr>
 
 
-<tr> <td>Select Month *</td>   <td> <select name=monthName id=monthName><option value='*'>Every Month</option>
-   <?php for ($ii = 1; $ii <= 12; $ii++) {
-         echo "<option value='";
-        	 echo $ii; 
-        	echo "'>";
-        	 echo $ii; 
-        	echo "</option>";
-        	  } ?>
-        
+
+            <tr> <td>Select Hours *</td>   <td> <select name=cronHour id=cronHour><option value='555' required>Every Hours</option>
+            <option value="1">1:AM</option>
+            <option value="2">2:AM</option>
+            <option value="3">3:AM</option>
+            <option value="4">4:AM</option>
+            <option value="5">5:AM</option>
+            <option value="6">6:AM</option>
+            <option value="7">7:AM</option>
+            <option value="8">8:AM</option>
+            <option value="9">9:AM</option>
+            <option value="10">10:AM</option>
+            <option value="11">11:AM</option>
+            <option value="12">12:AM</option>
+            <option value="13">1:PM</option>
+            <option value="14">2:PM</option>
+            <option value="15">3:PM</option>
+            <option value="16">4:PM</option>
+            <option value="17">5:PM</option>
+            <option value="18">6:PM</option>
+            <option value="19">7:PM</option>
+            <option value="20">8:PM</option>
+            <option value="21">9:PM</option>
+            <option value="22">10:PM</option>
+            <option value="23">11:PM</option>
+            <option value="24">12:PM</option>
             </td> </tr>
 
 
-            <tr> <td>Select  Month Date *</td>   <td> <select name=monthDate id=monthDate><option value=*>Every date</option>
-   <?php for ($iii = 1; $iii <= 31; $iii++) {
-         echo "<option value='";
-        	 echo $iii; 
-        	echo "'>";
-        	 echo $iii; 
-        	echo "</option>";
-        	  } ?>
-        
-            </td> </tr>
 
-
-            <tr> <td>Select Date of Week *</td>   <td> <select name=dayOfWeek id=dayOfWeek><option value=*>Every Day of Week</option>
-            <option value="0">Sunday</option>
-            <option value="1">Monday</option>
-            <option value="2">Tuesday</option>
-            <option value="3">Wednesday</option>
-            <option value="4">Thrusday</option>
-            <option value="5">Friday</option>
-            <option value="6">Saturday</option>
-            </td> </tr>
-
-
-            <tr> <td>Select Hours *</td>   <td> <select name=monthDate id=monthDate><option value=*>Select Hours</option>
-     <?php for ($j = 1; $j <= 23; $j++) {
-         echo "<option value='";
-        	 echo $j; 
-        	echo "'>";
-        	 echo $j; 
-        	echo "</option>";
-        	  } ?>
-        
-            </td> </tr>
-
-
-            <tr> <td>Select Minutes *</td>   <td> <select name=dayOfWeek id=dayOfWeek><option value=0>Select Minutes</option>
-            <option value="0">00</option>
+            <tr> <td>Select Minutes *</td>   <td> <select name=cronMinuts id=cronMinuts required><option value='' >Select Minutes</option>
+            <option value="00">00</option>
             <option value="10">10</option>
             <option value="20">20</option>
             <option value="30">30</option>
@@ -103,13 +114,23 @@ $prows = $result->fetch_all(MYSQLI_ASSOC);
 
 
 
-          
-        
-        
-        
-        
-        
-            <tr align="center"><td colspan ="2"><input type=submit name=rollback value="Create Cron" ></td></tr>
+            <tr> <td>Select Your Time Zone *</td>   <td> <select name=timeZone id=timeZone required><option value='' > Select Time Zone </option>
+       <?php
+           
+            $tzlist = DateTimeZone::listIdentifiers(DateTimeZone::ALL);
+            foreach($tzlist as $value)
+            {
+            echo "<option value=". $value .">". $value ."</option>";
+            }
+            echo "<select>";
+            ?>
+            </td> </tr>
+
+
+
+
+
+            <tr align="center"><td colspan ="2"><input type=submit name=createCronJob value="Create Cron"  onclick="return confirm('Are you sure want to create this CRON?')"></td></tr>
 </form>
 </center>
   
---
+++ vcimsweb/adminpanel/mangopinion/cronexcute.php
@@ -0,0 +1,152 @@
+<?php
+///////////////////////////////DATABASE CONNECTION ///////////////////
+        $dbhost = 'localhost';
+         $dbuser = 'root';
+         $dbpass = '';
+		 $dbname = 'vcims';
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $mysqli = new mysqli($dbhost,  $dbuser,  $dbpass,  $dbname);
+//////////////////////////////END DATA BASE CONNECTION ////////////////////
+ 
+
+
+if(!isset($CronRUN))
+{
+
+
+$gcmID = array();
+$qrr="SELECT * from cron_job";
+$result = $mysqli->query($qrr);
+
+if($result->num_rows > 0){  ///////  FIRST QUERY FROM THE CRON JOB TABLE
+$today = date('Y-m-d');
+    $prows = $result->fetch_all(MYSQLI_ASSOC);
+    foreach($prows as $result){   //////////////// FIRST FOREACH START
+
+    $projID      =    $result['project_id'];
+    $projStatus  =    $result['project_status'];
+    $projminutes =    $result['minutes'];
+    $projhour    =    $result['hour'];
+    $projmessage =    $result['message'];
+    $projtzlist  =    $result['tzlist'];
+if($projStatus == 0){
+    $qrr2="SELECT appuser_project_map.appuser_id,gcm_users.gcm_regid,appuser_project_map.start_date,appuser_project_map.exp_date FROM appuser_project_map inner join gcm_users on appuser_project_map.appuser_id= gcm_users.user_id WHERE appuser_project_map.project_id = $projID and appuser_project_map.status = 0 and appuser_project_map.exp_date >= '$today'";
+}else{
+    $qrr2="SELECT appuser_project_map.appuser_id,gcm_users.gcm_regid,appuser_project_map.start_date,appuser_project_map.exp_date FROM appuser_project_map inner join gcm_users on appuser_project_map.appuser_id= gcm_users.user_id WHERE appuser_project_map.project_id = $projID and appuser_project_map.status > 0 and appuser_project_map.exp_date >= '$today'";   
+}
+$resultQR = $mysqli->query($qrr2);
+if($resultQR->num_rows > 0){   ////3rd result querty start
+    $Resrows = $resultQR->fetch_all(MYSQLI_ASSOC);
+
+    foreach($Resrows as $GCMresult){ ////SECOUND FOR EACH START
+
+   $startDate = $GCMresult['start_date'];
+   $endDate = $GCMresult['exp_date'];
+   $gcmID[]= $GCMresult['gcm_regid']; /////TAKING GCM ID FROM THE TABLE
+   if(strtotime($today) >= strtotime($startDate)  &&  strtotime($today) <= strtotime($endDate)  ){
+
+    $loDate = $today.' '.$projhour.':'.$projminutes;
+
+    //  echo  $loDate; echo "<br/>";
+
+
+   }
+
+    
+    } ////// END SECOUND FOREACH
+    $messageTime=  strtotime($loDate); 
+       
+    
+    $dateTime =$loDate; 
+   // $tz_from = date_default_timezone_get();
+    $newDateTime = new DateTime($dateTime, new DateTimeZone($projtzlist)); 
+    $newDateTime->setTimezone(new DateTimeZone("UTC")); 
+    $dateTimeUTC = $newDateTime->format("Y-m-d H:i");
+
+    
+$serverDate = date('Y-m-d H:i');
+$serveTime=  strtotime($serverDate); 
+$givenTime=  strtotime($dateTimeUTC); 
+
+if($serveTime == $givenTime){///////FOR SENDING THE NOTIFICATION START
+    $succ  =	sendFCMnotification($$gcmID, $projmessage);
+
+
+}/////////////////END SENDING THE NOTIFICATION 
+
+
+}///// 3rd  result querty end
+    
+
+       // print_r($result); echo "<br/>"; echo "<br/>";
+
+
+    }////////////////// FIRST FOREACH END
+
+}/////////////////  FIRST QUERY FROM CRON IS ENDED HERE
+
+//print_r($gcmID);
+
+}/// END THE IF ISSET STATEMENT
+
+function sendFCMnotification($gcmId, $nofifyMsg ){
+	
+    $url ='https://fcm.googleapis.com/fcm/send';
+    $apikey ='AAAAu5kGXG0:APA91bG43dL7c2SAU4jZGoZLGI0Sw9d4RK03jo2nIUJaGhYlP274U8Xql7ikj9sg-pwgANz7JAI3lU64hN0uluvVVuIOIHhAApqHHgyv-u3TXSIilyuJWGazA54wHnuWBCCtc0Eoekiw';
+
+$header =array(
+    'Authorization:key='.$apikey,
+    'Content-Type:application/json'
+      );
+      
+      $nofifyData =[
+      'title'=>'Servegeygenics Message for you',
+       'body'=>$nofifyMsg,
+       'click_action'=> 'activity.notifyHandler'
+     
+      ];
+     
+    $data = array
+    (
+      'message'    => 'This message from Servegenics App',
+       'title'     => 'This is Servegenics Title',
+       'vibrate'   => 1,
+       'sound'     => 1,
+       
+    );
+
+
+        // CREATING NEW API BODY 
+
+        $nofifyBody = [
+            
+            'registration_ids' => $gcmId,
+            'notification'=> $nofifyData,
+            'data'=> $data,
+            'time_to_live'=> 3600
+            
+            ];
+
+
+
+      
+$ch =curl_init();
+curl_setopt($ch,CURLOPT_URL,$url);	  
+curl_setopt($ch,CURLOPT_POST,true);	  
+curl_setopt($ch,CURLOPT_HTTPHEADER,$header);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true );
+curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false );	  
+curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($nofifyBody));
+$result = curl_exec($ch);
+   
+if ($result === FALSE) {
+    die('Error in sending notification: ' . curl_error($ch));
+} 
+curl_close($ch);
+return $result;
+
+}
+
+
+
+?>
